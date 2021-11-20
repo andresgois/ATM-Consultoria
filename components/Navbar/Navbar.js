7a3886151f3f66c902b14631579d15c79ade95f5
Andre gois <andre.s.gois3@gmail.com>
2021-11-20 17:51:40 -0300

    tweaking navbar and creating your stylesheet

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
 
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { styles } from './Styles.module';
 
 class Navbar extends Component {
 
  render(){
    return(
-     <View>
-       <Text>
-         Hello World
+     <View style={styles.container}>
+       <Text style={styles.texto}>
+         ATM Consultoria
        </Text>
      </View>
    );
---
+++ components/Navbar/Styles.module.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    backgroundColor: '#ccc',
+    padding: 10,
+    height: 60,
+  },
+  texto: {
+    color: '#000',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  }
+});
