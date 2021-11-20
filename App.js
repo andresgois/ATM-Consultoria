43411da054760a36ed2e54df2674e73168d7c75f
Andre gois <andre.s.gois3@gmail.com>
2021-11-20 20:36:56 -0300

    starting the navigation between scenes part

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import React, {Component} from "react";
 import CenaClientes from "./components/Clientes/CenaClientes";
 import CenaPrincipal from "./components/Principal/CenaPrincipal";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
-
+const Stack = createNativeStackNavigator();
 export default class App extends Component {
   render(){
     return(
       // <CenaPrincipal />
-      <CenaClientes />
+      //<CenaClientes />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="Home"
+            component={CenaPrincipal}            
+            options={{ title: '' }}
+          />
+          <Stack.Screen 
+            name="Clientes"
+            component={CenaClientes}    
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
--- components/Principal/CenaPrincipal.js
+++ components/Principal/CenaPrincipal.js
@@ -1,8 +1,9 @@
 
 import React, { Component } from 'react';
-import { Image, StatusBar, View } from 'react-native';
+import { Image, StatusBar, TouchableOpacity, View } from 'react-native';
 import Navbar from "../Navbar/Navbar";
 import { styles } from './Styles.module';
+import { useNavigationContainerRef} from '@react-navigation/native'
 
 const logo        = require('../../imgs/logo.png');
 const menuCliente = require('../../imgs/menu_cliente.png');
@@ -10,21 +11,23 @@ const menuContato = require('../../imgs/menu_contato.png');
 const menuEmpresa = require('../../imgs/menu_empresa.png');
 const menuServico = require('../../imgs/menu_servico.png');
 
+const navigationRef = useNavigationContainerRef();
 class CenaPrincipal extends Component {
 
  render(){
    return(
     <View>
-      <StatusBar 
-        hidden={false}
-        backgroundColor='#ccc'
-      />
+      <StatusBar hidden={false} backgroundColor='#ccc' />
       <Navbar cor="#ccc" />
 
       <Image style={styles.logo} source={logo} />
 
       <View style={styles.grupoImg}>
-        <Image style={styles.menu} source={menuCliente} />
+        <TouchableOpacity
+          onPress={() => navigationRef.navigate('Clientes')}
+        >
+          <Image style={styles.menu} source={menuCliente} />
+        </TouchableOpacity>
         <Image style={styles.menu} source={menuContato} />
         <Image style={styles.menu} source={menuEmpresa} />
         <Image style={styles.menu} source={menuServico} />
